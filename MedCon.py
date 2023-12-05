84c45cf8705fef7c03fb3697ab8a496758e7d49f
oMarquess <pymasterclass@gmail.com>
2023-12-05 13:51:03 +0000

    stepup

---
+++ MedCon.py
@@ -0,0 +1,12 @@
+import os
+import openai
+import sys
+#sys.path.append('../..')
+
+import panel as pn  # GUI
+pn.extension()
+
+from dotenv import load_dotenv, find_dotenv
+_ = load_dotenv(find_dotenv()) # read local .env file
+
+openai.api_key  = os.environ['OPENAI_API_KEY']
